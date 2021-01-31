217c583f3319db60482718c144c7e2d9628869a1
Volkan Temel <68102722+VolkanTemel@users.noreply.github.com>
2021-01-31 14:21:41 +0300

    Enter

--- src/components/products/product/Product.js
+++ src/components/products/product/Product.js
@@ -7,14 +7,22 @@ class Product extends Component {
         oranges: [
             { id: 1, price: 10, title: "Washington Portakal" },
             { id: 2, price: 5, title: "Yerli Portakal" } ],
-        loaded: false
+        loaded: false,
+        cartCounter: 0
     };
 
     componentDidMount() {
         // await this.getData();
         this.setState({ loaded: true });
 
-    }
+    };
+
+    addToCart = () => {
+        let sum = this.state.cartCounter;
+        sum = sum + 1;
+        this.setState({ cartCounter: sum })
+        console.log(sum);
+    };
 
     // getData = async () => {
     //     await axios.get(`Portakallar.json`)
@@ -30,7 +38,7 @@ class Product extends Component {
                 <img src={ image } alt={ 'product' } />
                 <p>{ orange.title }</p>
                 <span>{ orange.price },00 TL</span>
-                <button>Sepete Ekle</button>
+                <button onClick={ this.addToCart }>Sepete Ekle</button>
             </li>
 
         )) : <h1>Loading...</h1>)
@@ -38,7 +46,7 @@ class Product extends Component {
         return (
             <div>
                 { renderItems }
-            </div >
+            </div>
         );
     };
 };
